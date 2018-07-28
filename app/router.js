21e137088cf8295e25abfbf3fc62119ab840f5df
Postnikov Maksim <postnikovmaksim@gmail.com>
2018-07-28 11:33:20 +0300

    fix return

--- app/router.js
+++ app/router.js
@@ -32,17 +32,18 @@ async function createBillAction({ sessionContext }) {
     const command = sessionContext.requests[sessionContext.requests.length - 1].command;
 
     const kontragentName = await identifyService.identifyKontragent({ command }).catch(e => e);
-    if (!kontragentName) Promise.resolve({ text: 'К сожалению не удалосьпонять имя контрагента, попробуйте еще раз' });
+    if (!kontragentName) return Promise.resolve({ text: 'К сожалению не удалось понять имя контрагента, попробуйте еще раз' });
 
     const kontragents = await kontragentService.getKontragent({ name: kontragentName }).catch(e => e);
-    if (kontragents.length === 0) Promise.resolve({ text: 'Контрагент с таким именем не найден, попробуйте еще раз' });
-    if (kontragents.length > 1) Promise.resolve({ text: 'К сожалению не полуться создать счет, так как контрагентов с таким именем больше одного. Могу ли я чем-то еще помочь?' });
+    if (kontragents.length === 0) return Promise.resolve({ text: 'Контрагент с таким именем не найден, попробуйте еще раз' });
+    if (kontragents.length > 1) return Promise.resolve({ text: 'К сожалению не полуться создать счет, так как контрагентов с таким именем больше одного. Могу ли я чем-то еще помочь?' });
 
     const productName = await identifyService.identifyProduct({ command }).catch(e => e);
-    if (!productName) Promise.resolve({ text: 'Не удалось определить название товара, попробуйте еще раз' });
+    if (!productName) return Promise.resolve({ text: 'Не удалось определить название товара, попробуйте еще раз' });
+
     const products = await productService.getProduct({ name: productName }).catch(e => e);
-    if (products.length === 0) Promise.resolve({ text: 'Товар с таким названием не найден, попробуйте еще раз' });
-    if (products.length > 1) Promise.resolve({ text: 'К сожалению не полуться создать счет, так как товаров с таким названием больше одного. Могу ли я чем-то еще помочь?' });
+    if (products.length === 0) return Promise.resolve({ text: 'Товар с таким названием не найден, попробуйте еще раз' });
+    if (products.length > 1) return Promise.resolve({ text: 'К сожалению не полуться создать счет, так как товаров с таким названием больше одного. Могу ли я чем-то еще помочь?' });
 
     await billService.create({kontragent: kontragents[0], products:[products[0]]});
     return Promise.resolve({ text: 'Счет создан, что-то еще?' });
