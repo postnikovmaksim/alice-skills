76c31fc372a7cd3440350719e991575757d812e5
Postnikov Maksim <postnikovmaksim@gmail.com>
2018-07-29 23:52:24 +0300

    добавил ударения и паузы во всех диалогах

--- app/router.js
+++ app/router.js
@@ -39,33 +39,65 @@ async function createBillAction({ sessionContext }) {
     const command = sessionContext.requests[sessionContext.requests.length - 1].command;
 
     const kontragentName = await identifyService.identifyKontragent({ command }).catch(e => e);
-    if (!kontragentName) return Promise.resolve({ text: 'К сожалению не удалось понять имя контрагента, попробуйте еще раз' });
+    if (!kontragentName) return Promise.resolve({
+        text: 'К сожалению не удалось понять имя контрагента, попробуйте еще раз',
+        tts: 'К сажал+ению - не удал+ось пон+ять +имя контраг+ента. - - Попробуйте еще раз.'
+    });
 
     const kontragents = await kontragentService.getKontragent({ fullName: kontragentName }).catch(e => e);
-    if (kontragents.length === 0) return Promise.resolve({ text: 'Контрагент с таким именем не найден, попробуйте еще раз' });
-    if (kontragents.length > 1) return Promise.resolve({ text: 'К сожалению не полуться создать счет, так как контрагентов с таким именем больше одного. Могу ли я чем-то еще помочь?' });
+    if (kontragents.length === 0) return Promise.resolve({
+        text: 'с таким именем контрагент не найден, попробуйте еще раз',
+        tts: '- с таким именем - контраг+ент не найден. - - попробуйте еще раз'
+    });
+    if (kontragents.length > 1) return Promise.resolve({
+        text: 'Найдено несколько контрагентов с похожим именем. Пока что не получиться создать счет, так как я не умею выбирать. Могу ли я чем-то еще помочь?',
+        tts: 'Найдено н+есколько контраг+ентов с похожим именем. - - Пока что не пол+учиться создать счет, - так как я не умею выбирать. - - Могу ли я чем-то еще помочь?'
+    });
 
     const productName = await identifyService.identifyProduct({ command }).catch(e => e);
-    if (!productName) return Promise.resolve({ text: 'Не удалось определить название товара, попробуйте еще раз' });
+    if (!productName) return Promise.resolve({
+        text: 'Не удалось определить название товара, попробуйте еще раз',
+        tts: 'Не удал+ось определ+ить назв+ание тов+ара. Попробуйте еще раз.'
+
+    });
 
     const products = await productService.getProduct({ name: productName }).catch(e => e);
-    if (products.length === 0) return Promise.resolve({ text: 'Товар с таким названием не найден, попробуйте еще раз' });
-    if (products.length > 1) return Promise.resolve({ text: 'К сожалению не полуться создать счет, так как товаров с таким названием больше одного. Могу ли я чем-то еще помочь?' });
+    if (products.length === 0) return Promise.resolve({
+        text: 'Товар с таким названием не найден, попробуйте еще раз',
+        tts: 'Товар - с так+им назв+анием не н+айден. - -Попр+обуйте еще раз.'
+    });
+    if (products.length > 1) return Promise.resolve({
+        text: 'К сожалению не полуться создать счет, так как товаров с таким названием больше одного. Могу ли я чем-то еще помочь?',
+        tts: 'К сажал+ению  - не пол+учится созд+ать счет, - так как тов+аров с так+им назв+анием б+ольше одног+о, - а я не ум+ею выбир+ать. - - Могу ли я чемто еще помочь?'
+    });
 
     const bill = await billService.create({kontragent: kontragents[0], products:[products[0]]});
     sessionContext.lastCreateDocument.push(bill);
-    return Promise.resolve({ text: 'Счет создан, что-то еще?' });
+    return Promise.resolve({
+        text: 'Счет создан, что-то еще?',
+        tts: 'Счет создан. - - - Чтото ещё?'
+    });
 }
 
 async function createSendEmail({ sessionContext }) {
     await emailService.send({ sessionContext });
-    return Promise.resolve({ text: 'Отправила, еще что-то?' });
+    return Promise.resolve({
+        text: 'Отправила, еще что-то?',
+        tts: 'Отпр+авила. - - - Ещё чтото?'
+    });
 }
 
 function closeSkillsAction() {
-    return Promise.resolve({ text: 'Приятно было пообщаться.', end_session: true });
+    return Promise.resolve({
+        text: 'Приятно было пообщаться.',
+        tts: 'Приятно было пообщ+аться',
+        end_session: true
+    });
 }
 
 function notUnderstandAction() {
-    return Promise.resolve({ text: 'К сожалению, я Вас не поняла. Повторите пожайлуста.' });
+    return Promise.resolve({
+        text: 'К сожалению, я Вас не поняла. Повторите пожайлуста',
+        tts: 'К сожал+ению - - я Вас не поняла. Повторите паж+алуста.'
+    });
 }
